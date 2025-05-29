dd869166488c879cdca06e85a5afb330f5660f88
hebertsandinha <hebertsandinha@gmail.com>
2025-05-29 08:29:55 -0300

    .

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/userController');
+const usuarioController = require('../controllers/usuarioController');
 const { protect, restrictToAdmin } = require('../middlewares/authMiddleware');
 
 
-router.get('/me', protect, userController.getMe); 
-router.put('/me', protect, userController.updateMe);
-router.get('/', protect, restrictToAdmin, userController.getAllUsers);
-router.get('/:id', protect, restrictToAdmin, userController.getUserById);
-router.put('/:id', protect, restrictToAdmin, userController.updateUserByAdmin);
-router.delete('/:id', protect, restrictToAdmin, userController.deleteUserByAdmin); 
+router.get('/me', protect, usuarioController.getMe); 
+router.put('/me', protect, usuarioController.updateMe);
+router.get('/', protect, restrictToAdmin, usuarioController.getAllUsers);
+router.get('/:id', protect, restrictToAdmin, usuarioController.getUserById);
+router.put('/:id', protect, restrictToAdmin, usuarioController.updateUserByAdmin);
+router.delete('/:id', protect, restrictToAdmin, usuarioController.deleteUserByAdmin); 
 
 
 module.exports = router;
